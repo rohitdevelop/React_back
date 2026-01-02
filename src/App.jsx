1dd86b4a42a85adbbbb7a10b10fec8665a3af656
Rohit singh <rohitdev124421@gmail.com>
2026-01-02 17:07:30 +0530

    local storage done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
  import React from 'react'
-  import  Card  from "./components/Card.jsx";
- const App = () => {
-   return (
+ import { axios } from "axios";
+  // import  Card  from "./components/Card.jsx";
+  const App = () => {
+ 
+   async function handle() {
+      await axios("deta aa gya")
+    }
+ 
+ return (
      <div>
-       <Card />
-     </div>
+      {/* <Card /> */}
+<button onClick={handle} className="bg-amber-900 text-amber-50 py-4 px-4">click me </button>
+      {/* <h1>hello </h1> */}
+      </div>
    )
  }
  
